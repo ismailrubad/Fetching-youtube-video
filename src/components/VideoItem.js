6d73461d27cc3bb5369b47ce025d9a18746c9ed8
Rubad <rubad.hossain@gmail.com>
2019-04-16 14:54:05 +0600

    Nested Callbacks

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,13 @@
+import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = (props) => {
     return(
-        <div>
-            <img src = {props.video.snippet.thumbnails.medium.url} />
+        <div onClick={() => props.onVideoSelect(props.video)} className="video-item item">
+            <img className="ui image" src = {props.video.snippet.thumbnails.medium.url} />
+            <div className="content">
+                <div className="header">{props.video.snippet.title}</div>
+            </div>
         </div>
     );
 }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = (props) => {
-
+    
     const rederedList = props.videos.map((video) => {
-        return <VideoItem video={video}  />
+        return <VideoItem video={video} onVideoSelect={props.onVideoSelect} />
     });
 
-    return <div>{rederedList}</div>
+    return <div className="ui relaxed divided list">{rederedList}</div>
 }
 
 export default VideoList;
