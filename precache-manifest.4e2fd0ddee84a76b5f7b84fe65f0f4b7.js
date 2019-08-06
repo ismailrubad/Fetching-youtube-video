e74fc785cc523f5708110fe0a680d97373ed49d8
Rubad <rubad.hossain@gmail.com>
2019-08-06 15:24:27 +0600

    Updates

---
+++ precache-manifest.4e2fd0ddee84a76b5f7b84fe65f0f4b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "462cd137f99872d77971",
+    "url": "/Fetching-youtube-video/static/js/runtime~main.61899234.js"
+  },
+  {
+    "revision": "5a8079024c124ccd758f",
+    "url": "/Fetching-youtube-video/static/js/main.62515946.chunk.js"
+  },
+  {
+    "revision": "6b7a8549d14891e00d91",
+    "url": "/Fetching-youtube-video/static/js/2.1e7b24b2.chunk.js"
+  },
+  {
+    "revision": "5a8079024c124ccd758f",
+    "url": "/Fetching-youtube-video/static/css/main.0fac5d1d.chunk.css"
+  },
+  {
+    "revision": "fb4c566dd5333bedfabc424d68bc2fe8",
+    "url": "/Fetching-youtube-video/index.html"
+  }
+];
